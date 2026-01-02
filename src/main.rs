8b942e66c3bbfd7b9f4239dd34dc681241ba5a1f
Kurt DEGLA <kurtdegla@gmail.com>
2026-01-03 00:10:02 +0100

    Lol

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
 
      print!("$ ");
      io::stdout().flush().unwrap();
-     let mut command = String::new()
+     let mut command = String::new();
      io::stdin()::read_line(&mut command).unwrap();
-     println!("{}: command not found", command.trim())
+     println!("{}: command not found", command.trim());
 }
 
