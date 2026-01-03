c8ddbeb90095c7dabbd8051cfba8cc3380dd57f8
Kurt DEGLA <kurtdegla@gmail.com>
2026-01-03 01:09:26 +0100

    Lol

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,28 @@ fn main() {
      io::stdout().flush().unwrap();
      let mut command = String::new();
      io::stdin().read_line(&mut command).unwrap();
-     if command.trim() != "exit" { // if command != "exit\n"
-     println!("{}: command not found", command.trim());
+    let command_line = command.trim();
+
+    if command_line.is_empty(){continue};
+
+    let command_part : Vec<&str> = command_line.split_whitespace().collect();
+
+    let command = command_part[0];
+
+
+     if command == "exit" { // if command == "exit\n"
+        break
+     }
+     else if command == "echo" {
+// On prend tous les mots APRES le premier et on les recolle avec un espace
+        let message = &command_part[1..].join(" ");
+        println!("{}", message);
         continue
-     } else
-     {break};
+     } 
+     else
+     {     
+        println!("{}: command not found", command.trim());
+        };
 
  }
 }
