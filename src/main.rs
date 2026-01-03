c1de23ee3f12e9d185fcbd263f9f5971127d1cd9
Kurt DEGLA <kurtdegla@gmail.com>
2026-01-03 01:38:22 +0100

    Lol

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,47 @@ fn main() {
      io::stdin().read_line(&mut command).unwrap();
     let command_line = command.trim();
 
+    // On liste ici toutes les commandes "intégrées" (built-ins)
+    let commandes_internes = ["exit", "echo", "type"];
+
     if command_line.is_empty(){continue};
+
+
 
     let command_part : Vec<&str> = command_line.split_whitespace().collect();
 
     let command = command_part[0];
 
+    let lenght = command_part.len();
 
-     if command == "exit" { // if command == "exit\n"
-        break
-     }
-     else if command == "echo" {
-// On prend tous les mots APRES le premier et on les recolle avec un espace
-        let message = &command_part[1..].join(" ");
-        println!("{}", message);
-        continue
-     } 
-     else
-     {     
-        println!("{}: command not found", command.trim());
+
+    match command {
+        "exit" => {
+            break
+        }
+        "echo" => {
+            let message = command_part[1..].join(" ");
+            println!("{}", message);
+        }
+        "type" => {
+            if lenght < 2 {
+                println!("type : missing argument");
+            } else {
+            let command_check = command_part[1];
+            
+            if commandes_internes.contains(&command_check){
+                println!("{} is a builtin shell",command_check);
+            }else{
+                println!("{}: command not found", command_check);
+            }
         };
+        }
+        // Le "_" veut dire "tout le reste" (l'erreur)
+        _ => {
+            println!("{}: command not found", command);
+        }
 
- }
+    }
+
+    }
 }
