7cfa43e918b62a8cc001728d2cae64fcd0710a96
Kurt DEGLA <kurtdegla@gmail.com>
2026-01-05 15:54:34 +0100

    cd expanding LOl

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,17 @@ fn main() {
 
     // On liste ici toutes les commandes "intégrées" (built-ins)
 
-
+// Ligne de commande vide 
     if command_line.is_empty(){continue};
 
 
-
+// segmenter l'entrée en paramtètrepar un espace
     let command_part : Vec<&str> = command_line.split_whitespace().collect();
 
+// première valeurest la commande
     let command = command_part[0];
 
+//Le nombre de paramètre
     let lenght = command_part.len();
 
 
@@ -55,11 +57,11 @@ fn main() {
             if lenght < 2 {
                 println!("type : missing argument");
             }  else {
-                let command_check = command_part[1];
+                let command_check = command_part[1]; // premier argument de type 
                 if BUILTIN_COMMANDS.contains(&command_check) {
                         println!("{} is a shell builtin", command_check);
-                    } else if let Some(path) = find_executable_in_path(command_check) {
-                        println!("{} is {}", command_check, path.to_str().unwrap().to_string());
+                    } else if let Some(path) = find_executable_in_path(command_check) { // verifier si command_check existent dand le Path et que ca retourne quelque chose
+                        println!("{} is {}", command_check, path.to_str().unwrap().to_string()); // ffciher command et le path retourné si Oui 
                     } else {
                         println!("{}: not found", command_check);
                     }
@@ -73,7 +75,7 @@ fn main() {
                 continue;
             } else {
                 match  env::current_dir() {
-                    Ok(path) => println!("{}",path.display()),
+                    Ok(path) => println!("{}",path.display()), // Répertoire courant si OK 
                     Err(_) => println!("execution failed")
                 }
             }
@@ -86,13 +88,13 @@ fn main() {
                 continue;
             } else {
    
-                let dir_att = command_part[1..].join("");
+                let dir_att = command_part[1..].join(""); // arugment de cd 
 
                 match dir_att.as_str()
                 {           
                     "~" => {
-                            if let Some(path) =  env::home_dir(){
-                                match env::set_current_dir(path) {
+                            if let Some(path) =  env::home_dir(){ // SI repertoire HOME est retourné 
+                                match env::set_current_dir(path) { // Y aller dedans 
                                 Ok(_good) => continue,
                                 Err(_) => println!("Home directory not found")
                                 }
@@ -101,7 +103,7 @@ fn main() {
                     _ => {
                             let path = Path::new(&dir_att);
 
-                            match env::set_current_dir(path) {
+                            match env::set_current_dir(path) { // Aller dans le repertoire path si existant
                                 Ok(_good) => continue,
                                 Err(_) => println!("cd: {}: No such file or directory",path.display())
                             }
@@ -111,11 +113,11 @@ fn main() {
         }
         // Le "_" veut dire "tout le reste" (l'erreur)
         _ => {
-            if let Some(_path) = find_executable_in_path(command){
-                let status = Command::new(command)
-                .args(&command_part[1..])
-                .spawn()
-                .and_then(| mut child| child.wait());
+            if let Some(_path) = find_executable_in_path(command){ // verifier si command_check existent dand le Path et que ca retourne quelque chose
+                let status = Command::new(command) //intialiser command 
+                .args(&command_part[1..])   //récupérer l'arguemnt de la command
+                .spawn()    // l'exécuter 
+                .and_then(| mut child| child.wait());   // Dire au programme parent ( du shell ) d'attendre l'execution de cette command spawner
                 
                 if let Err(_) = status {
                     println!("{}: execution failed", command );
